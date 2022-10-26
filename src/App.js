dcc27d625561468fb5ea45fa11986ba92f17ef66
lilbobb <ukurowo@gmail.com>
2022-10-26 16:55:59 +0100

    Added error boundary and 404 page

--- src/App.js
+++ src/App.js
@@ -5,33 +5,23 @@ import About from "./components/About";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
 import Users from "./components/Users";
-import ErrorBound from "./components/ErrorBound";
 import ErrorHandler from "./components/ErrorBound";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            Home page
-          </Route>
-          <Route path="/About" element={<About />}>
-            About page
-          </Route>
-          <Route path="/Login" element={<Login />}>
-            Login Page
-          </Route>
-          <Route path="/SignUp" element={<SignUp />}>
-            SignUp Page
-          </Route>
-          <Route path="/Users" element={<Users />}>
-            USers Page
-          </Route>
-        </Routes>
-      </Router>
       <ErrorHandler>
-        <ErrorBound />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/About" element={<About />}></Route>
+            <Route path="/Login" element={<Login />}></Route>
+            <Route path="/SignUp" element={<SignUp />}></Route>
+            <Route path="/Users" element={<Users />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </Router>
       </ErrorHandler>
     </div>
   );
